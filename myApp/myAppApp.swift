49645b5eec3281f8786c73049a655c6a0a8e9a97
Kyeicus <kyeicusnketia@gmail.com>
2021-06-15 10:09:56 +0000

    Initial Commit

---
+++ myApp/myAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAppApp.swift
+//  myApp
+//
+//  Created by Kyeicus 😋 on 15/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct myAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
